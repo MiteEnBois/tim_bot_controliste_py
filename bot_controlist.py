1e60b4d7a36adc25c45c4be86aed3b5439d86a58
Tim <mystergreen045@gmail.com>
2020-01-30 21:39:55 +0100

    plein d'ajouts

--- bot_controlist.py
+++ bot_controlist.py
@@ -1,33 +1,53 @@
 # bot.py
-import os
 
+import os
+import json
+import yaml
+import datetime
 import discord
+import time
+from copy import deepcopy
 from discord.ext import commands
 from dotenv import load_dotenv
-import json
-import datetime
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 bot = commands.Bot(command_prefix='!')
 
-with open('roles.json') as json_file:
-    global roles_list
-    roles_list = json.load(json_file)["role_list"]
+global PATH, BIGDATA, MAX_BEFORE_BACKUP, MAX_BEFORE_MSG, ROLES_LIST, MANUAL_BACKUP
+
+MANUAL_BACKUP = False
+PATH = 'score.yml'
+MAX_BEFORE_BACKUP = 30*60
+MAX_BEFORE_MSG = 5
+
+
+with open(PATH) as f:
+    data = yaml.load(f, Loader=yaml.FullLoader)
+    BIGDATA = data
+
+    ROLES_LIST = []
+    for n in data:
+        ROLES_LIST.append(n["name"])
+
+
+def backup_score():
+    with open('score.yml', mode="r+") as f:
+        data = yaml.dump(BIGDATA, f)
 
 
 def check_roles(member):
     i = 0
     for r in member.roles:
-        if r.name in roles_list:
+        if r.name in ROLES_LIST:
             i += 1
     return i
 
 
 def get_rol(member):
     for r in member.roles:
-        if r.name in roles_list:
+        if r.name in ROLES_LIST:
             return r
     return None
 
@@ -45,7 +65,7 @@ async def list_role(ctx, *arr):
         txt = "__**Liste des equipes**__\n"
         i = 0
         for r in roles:
-            if r.name in roles_list:
+            if r.name in ROLES_LIST:
                 if i == 0:
                     txt += ":trophy: "
                 txt += "**"+r.name + "**"
@@ -67,7 +87,7 @@ async def list_role(ctx, *arr):
             if test in "team":
                 break
             if (test in name) or (test == r.mention):
-                if r.name in roles_list:
+                if r.name in ROLES_LIST:
                     role = r
                     i += 1
         if i == 0:
@@ -87,11 +107,47 @@ async def list_role(ctx, *arr):
 
 
 @bot.command(name='ping', help='Pong!')
-async def ping(ctx, *arr):
+async def ping(ctx):
     await ctx.send("Pong!")
 
 
-@bot.command(name='rejoin', help="Permet de faire rejoindre dans sa propre team. Ne marche que si l'auteur de la commande est dans une team et si l'invité n'en a pas")
+def sort_data(d):
+    return d["score"]
+
+
+@bot.command(name='score', help='Affiche le score actuel de chaque equipe')
+async def score(ctx):
+    copydata = deepcopy(BIGDATA)
+    copydata.sort(key=sort_data, reverse=True)
+    txt = "**__Liste des scores__ :** \n"
+    i = 0
+    for d in copydata:
+        txt += " - "
+        if i == 0:
+            txt += ":trophy: "
+        txt += "**"+d["name"] + "**"
+        if i == 0:
+            txt += " :trophy:"
+        txt += ": "+str(d["score"])+"\n"
+        i += 1
+    await ctx.send(txt)
+
+
+@bot.command(name='back', help='Enregistre le score actuel. Activé : '+str(MANUAL_BACKUP))
+async def backup(ctx):
+    if MANUAL_BACKUP:
+        backup_score()
+        await ctx.send("backed")
+
+
+@bot.command(name='limit', help='Affiche les différentes limites')
+async def limit(ctx):
+    txt = "Les backup des scores se font toute les "+str(MAX_BEFORE_BACKUP)+" secondes, quand un message est recu\n"
+    txt += "Limite antispam : "+str(MAX_BEFORE_MSG)+" secondes d'attente PAR EQUIPE avant de recevoir un point"
+    await ctx.send(txt)
+
+
+@bot.command(name='join', help="Permet de faire rejoindre dans sa propre team. Ne marche que si l'auteur de la commande est dans une team et si l'invité n'en a pas")
 async def rejoin(ctx, invite: discord.Member):
     auth = ctx.author
     if check_roles(auth) != 1:
@@ -128,6 +184,46 @@ async def degage(ctx, invite: discord.Member):
 @bot.event
 async def on_ready():
     print('bot started')
+
+
+def score_management(message):
+    role = get_rol(message.author)
+    if role is not None:
+        for d in BIGDATA:
+            if(role.name == d["name"]):
+                if(d["lastmsg"] is None):
+                    d["lastmsg"] = time.time()
+                lmsg = time.time()-d["lastmsg"]
+                if lmsg >= MAX_BEFORE_MSG:
+                    d["score"] += 1
+                    d["lastmsg"] = time.time()
+
+
+def reponses_timbot(message):
+    if "timbot" in message.content.lower():
+        if "merci" in message.content.lower():
+            return"Mais de rien, "+str(message.author.display_name)+"!"
+        if "hein" in message.content.lower() and "?" in message.content.lower():
+            if message.author.id == 123742890902945793:
+                return "Mais oui, évidemment "+str(message.author.display_name)+"!"
+            else:
+                return "Pourquoi tu me demande? J'ai l'air d'être ton ami?"
+
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    msg = reponses_timbot(message)
+    if msg is not None:
+        await message.channel.send(msg)
+    score_management(message)
+
+    back = time.time()-os.path.getmtime(PATH)
+    if back >= MAX_BEFORE_BACKUP:
+        backup_score()
+        print("backing")
+    await bot.process_commands(message)
 
 
 @bot.event
